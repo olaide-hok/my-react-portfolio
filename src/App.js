56631136ef11121370c1648f52a3d6b542cb0b4e
olaide-hok <olaidekareem43@yahoo.com>
2022-03-08 18:38:36 +0100

    Created folder structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+
 function App() {
   return (
-    <div className="">
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/projects' element={<Projects/>} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+
+    </Router>
+    
   );
 }
 
