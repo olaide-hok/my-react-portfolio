4df81e547963c0acde575089bc49b29a2fb49255
olaide-hok <olaidekareem43@yahoo.com>
2022-03-10 15:24:19 +0100

    Add Footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import {FaGithubSquare, FaTwitter, FaMailBulk} from 'react-icons/fa'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='bg-theme w-screen flex justify-center'>
+      <div className='md:w-full w-1/2'>
+        <div className="bg-theme p-10 text-center">
+          <p className="text-gray-50">Designed and Developed By</p>
+          
+          
+
+          <div className="flex text-white w-full justify-between">
+            <FaTwitter />
+            <FaMailBulk />
+            <FaGithubSquare />
+
+          </div>
+          <p className="text-gray-50">Kareem Habeeb</p>  
+        </div>
+      </div>
+    </div>
   )
 }
 
